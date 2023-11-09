5bce9df182313c76686c2fa934a9db1029c6b090
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-11-08 21:08:11 -0400

    getting `CNPJ` and `TotalPayable` from fullHTML

--- src/treatment.js
+++ src/treatment.js
@@ -1,6 +1,10 @@
 export function Treatment(text) {
   let fullHtml = text
-  let removingHead = fullHtml.replace(/[\s\S]*<head>[\s\S]*<\/head>/,'')
-  let supermarketName = removingHead.match(/<div class="txtTopo" id="u20">([^<]*)/)
+  // let removingHead = fullHtml.replace(/[\s\S]*<head>[\s\S]*<\/head>/,'')
+  let supermarketName = fullHtml.match(/<div class="txtTopo" id="u20">([^<]*)/)
+  let supermarketCNPJ = fullHtml.match(/CNPJ:\W*([^<]*)/)
+  let totalPayable = fullHtml.match(/txtMax">*([^<]*)/)
   console.log(supermarketName[1])
+  console.log(supermarketCNPJ[1])
+  console.log(totalPayable[1])
 }
